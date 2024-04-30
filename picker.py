3e51301fe0e07f11a2dbb920b1bac271c2cae51b
Sergio <sergiobluez@gmail.com>
2024-04-30 13:24:16 -0700

    Random picker for contests etc
    More functions coming soon, like catching bots or bad actors. This works however.

---
+++ picker.py
@@ -0,0 +1,10 @@
+import random
+
+# List of names from your contest
+contestants = ['Name1', 'Name2', 'Name3', 'Name4', ...]
+
+# Randomly select a winner
+winner = random.choice(contestants)
+
+# Print the winner
+print("The winner is:", winner)
